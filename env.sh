90a5f52110163ce6244f65e644e0ab89604a4d7b
Johan Jönsson <johanjoensson@gmail.com>
2022-09-23 12:53:17 +0900

    Do no overwrite PYTHONPATH, append to it. Also, make sure to add the base folder to the PYTHONPATH, not the parent.

--- env.sh
+++ env.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-source ~/env/bin/activate 
+. ~/PyRSPthon/bin/activate
 
 # Script folder
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# Parent folder
-DIR="${DIR}/../"
-export PYTHONPATH=$PYTHONPATH:$DIR
+export PYTHONPATH="$DIR:$PYTHONPATH"
 
